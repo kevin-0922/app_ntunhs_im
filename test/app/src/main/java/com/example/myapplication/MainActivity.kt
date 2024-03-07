f871b3229f2dc5116d3c72592efc09c1837a87ae
kevin-0922 <117199474+kevin-0922@users.noreply.github.com>
2024-03-07 16:55:41 +0800

    V2.0

--- test/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ test/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,9 +12,13 @@ import androidx.appcompat.app.AlertDialog
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG : String = MainActivity::class.java.simpleName
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
 
 
         val textView = findViewById<TextView>(R.id.textView)
@@ -22,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editText)
         var validate_num:Int
         var secret : Int = Random().nextInt(50)+1
+        var start:Int = 1
+        var end:Int = 100
 
         guess_button.setOnClickListener {
 
@@ -37,6 +45,16 @@ class MainActivity : AppCompatActivity() {
                 ans_str="你猜的比較小喔"
             }
             textView.text=ans_str
+
+            if(textView.text=="你猜對了喔"){
+                handler.postDelayed(
+                    {Toast.makeText(this, "6秒後重置結果!", Toast.LENGTH_SHORT).show()}, 6000
+                )
+                secret=Random().nextInt(10)+1
+                start=1
+                end=100
+                textView.text="我們再猜一次"
+            }
         }
 
         reset_button.setOnClickListener {
